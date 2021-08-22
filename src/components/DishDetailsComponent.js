93ebfb9d59567b35beba745035fb98f8f173cda1
Arece7 <arece73@gmail.com>
2021-08-22 22:01:30 +0530

    React component part 3

---
+++ src/components/DishDetailsComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { DISHES } from "../shared/dish";
+
+class DishDetails extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+          dishes : DISHES
+        };
+    }
+
+    renderDish(dish) {
+          return(
+              <Card>
+                  <CardImg top src={dish.image} alt={dish.name} />
+                  <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                  </CardBody>
+              </Card>
+          );
+  }
+
+  renderComments(comments){
+     return(
+      <div>
+        <h4>Comments</h4>
+        {comments.map(comment =>
+         <ul className="list-unstyled" key={comment.id}>
+          <li>{comment.comment}</li>
+          <li>--{comment.author},{new Intl.DateTimeFormat('en-US', { 
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric'
+            }).format(new Date(comment.date))}</li>
+        </ul>
+        )}
+    </div>
+     );
+  }
+
+    render(){
+      if(this.props.selectedDish != null)
+      return( 
+      <div className="row">
+           <div  className="col-12 col-md-5 m-1">
+              {this.renderDish(this.props.selectedDish)}
+            </div>
+            <div  className="col-12 col-md-5 m-1">
+            {this.renderComments(this.props.selectedDish.comments)}
+            </div>
+       </div>
+      );
+      else
+      return(
+        <div></div>
+      )
+  }
+  }
+  export default DishDetails;
