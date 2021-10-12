3ff9a72e38a3bd68bf2af08e845b191989d57e2e
Arece7 <arece73@gmail.com>
2021-10-12 23:25:23 +0530

    handling errors when server not responsive

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -57,7 +57,7 @@ componentDidMount() {
         <DishDetails dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
         isLoading={this.props.dishes.isLoading}
         ErrMess={this.props.dishes.errMess}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
           commentsErrMess={this.props.comments.errMess} addComment={this.props.addComment} />
     );
     }
